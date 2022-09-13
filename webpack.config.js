2de91801d35f7f42ab5f08b696986ea0516cb281
hwceong-edu <hceong@sfu.ca>
2022-09-12 23:12:29 -0700

    source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
-    output: { path: path.join(__dirname, "dist"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-    devServer: { 
-        static: path.join(__dirname, "src"),
-        port: 3000
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/env", "@babel/react"]
-                    }
-                } 
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src", "index.html"),
-        }),
-    ],
+module.exports = (env) => {
+    return {
+        entry: path.join(__dirname, "src", "index.js"),
+        output: { path: path.join(__dirname, "dist"), filename: "index.bundle.js" },
+        mode: env.dev ? "development" : "production",
+        devtool: env.dev ? "source-map" : "none",
+        resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+        devServer: { 
+            static: path.join(__dirname, "src"),
+            port: 3000
+        },
+        module: {
+            rules: [
+                { 
+                    test: /\.(js|jsx)$/, 
+                    exclude: /node_modules/, 
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/env", "@babel/react"]
+                        }
+                    } 
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "src", "index.html"),
+            }),
+        ],
+    }
 };
